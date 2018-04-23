1ccba466c0c559f858af0128ad1d90914d1162fa
EA <eabramovich@exadel.com>
2018-04-23 20:15:57 +0300

    file list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { File, Entry } from '@ionic-native/file';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    path: string = "Music/";
+    message: string;
+    filename: string;
+    filelist: Entry[];
 
-  }
+    constructor(public file: File) {
+        this.reloadPath();
+    }
 
+    reloadPath() {
+        console.log('-- reload path');
+        //let self = this;
+        this.file.resolveLocalFilesystemUrl(this.path).then(entry => {
+            //console.log('entry:',entry);
+            //self.filelist = entries; 
+        });
+
+    }
+    /*
+    listDir(path: string): Promise {
+        return this.file.resolveLocalFilesystemUrl(path);
+
+    }*/
 }
