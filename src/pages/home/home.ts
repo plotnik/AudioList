d6c8e3ca9560f75a52cc11579914a59078695dc6
Egor Abramovich <eabramovich@exadel.com>
2018-05-05 18:54:02 +0300

    generate

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { File, Entry } from '@ionic-native/file';
+import { AlertController } from 'ionic-angular';
 
 @Component({
     selector: 'page-home',
@@ -7,13 +8,23 @@ import { File, Entry } from '@ionic-native/file';
 })
 export class HomePage {
 
+    /** Path to the current folder */
     path: string = "Music";
-    message: string;
-    filename: string;
+
+    /** List of files in the current folder */
     filelist: Entry[];
+
+    /** Number of `.mp3` files in the current folder */
     numfiles: number;
 
-    constructor(public file: File) {
+    /** Name of playlist file to be generated */
+    playlist: string;
+
+    /** List of `.mp3` files including ones in subfolders */
+    allfiles: Entry[];
+
+    constructor(public file: File,
+        public alertCtrl: AlertController) {
         this.reloadPath();
     }
 
@@ -28,7 +39,6 @@ export class HomePage {
                     }
                     return entry.isDirectory;
                 });
-                //console.log('-- filelist.length:',this.filelist.length);
             });
     }
 
@@ -38,11 +48,81 @@ export class HomePage {
     }
 
     clickUp() {
-        //let k = this.path.substr(0, this.path.length - 1).lastIndexOf('/');
         let k = this.path.lastIndexOf('/');
         if (k > 0) {
             this.path = this.path.substring(0, k);
             this.reloadPath();
         }
+    }
+
+    generate() {
+        // read list of mp3 files recursively
+        this.allfiles = [];
+        this.listFilesRecursive(this.path).then(() => {
+            this.createPlaylist();
+        });
+    }
+
+    createPlaylist() {
+        console.log('createPlaylist');
+        // sort files in alphabetic order
+        this.allfiles.sort((a: Entry, b: Entry) => {
+            if (a.fullPath < b.fullPath) {
+                return -1;
+            } else
+                if (a.fullPath > b.fullPath) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+        });
+
+        // create file contents
+        var text = "#EXTM3U\n";
+        for (var i = 0; i < this.allfiles.length; i++) {
+            var f = this.allfiles[i];
+            text += "#EXTINF:-1," + f.name + "\n" +
+                f.fullPath.substring(this.path.length + 1) + "\n";
+        }
+        var fname = this.path + '/' + this.playlist + '.m3u8';
+        this.file.writeFile(this.file.externalRootDirectory, fname, text, { replace: true }).then(value => {
+            let alert = this.alertCtrl.create({
+                title: 'Playlist saved',
+                subTitle: fname + ' (' + this.allfiles.length + ' entries)',
+                buttons: ['OK']
+            });
+            alert.present();
+
+        }).catch(reason => {
+            let alert = this.alertCtrl.create({
+                title: 'Cannot save file',
+                subTitle: fname + ' (reason: ' + reason.message + ')',
+                buttons: ['OK']
+            });
+            alert.present();
+        });
+    }
+
+    listFilesRecursive(path: string): Promise<boolean> {
+        let self = this;
+        return new Promise((resolve, reject) => {
+            self.file.listDir(self.file.externalRootDirectory, path)
+            .then((entries: Entry[]) => {
+                var a = [];
+                entries.forEach((entry: Entry) => {
+                    if (entry.isDirectory) {
+                        a.push(this.listFilesRecursive(path + '/' + entry.name));
+                    } else
+                    if (entry.name.toLowerCase().endsWith('.mp3')) {
+                        self.allfiles.push(entry);
+                    }
+                });
+                Promise.all(a).then(() => {
+                    resolve(true);
+                })
+            }).catch(err => {
+                reject(err);
+            });
+        });
     }
 }
